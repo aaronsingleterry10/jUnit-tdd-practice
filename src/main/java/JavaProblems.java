f0e6f94a84a6faa04280095ff274abfd481e549c
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-01 10:02:46 -0500

    added testHelloWorld and testAddInts tests

--- src/main/java/JavaProblems.java
+++ src/main/java/JavaProblems.java
@@ -1,2 +1,10 @@
 public class JavaProblems {
+
+    public static String returnHelloWorld() {
+        return "Hello World";
+    }
+
+    public static int addInts(int n1, int n2) {
+        return n1 + n2;
+    }
 }
--- src/test/java/JavaProblemsTest.java
+++ src/test/java/JavaProblemsTest.java
@@ -1,2 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class JavaProblemsTest {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello World", JavaProblems.returnHelloWorld());
+    }
+
+    @Test
+    public void testAddInts() {
+        int expected = 10;
+        int actual = JavaProblems.addInts(5, 5);
+        assertEquals(expected, actual);
+    }
 }
