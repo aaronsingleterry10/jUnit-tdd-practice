7a33cc0c6143e6efcd990aef220284ca39511e39
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-01 14:05:48 -0500

    completed a test

--- src/main/java/JavaProblems.java
+++ src/main/java/JavaProblems.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Field;
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class JavaProblems {
 
     public static String returnHelloWorld() {
@@ -6,5 +9,20 @@ public class JavaProblems {
 
     public static int addInts(int n1, int n2) {
         return n1 + n2;
+    }
+
+    public static long intToLong(int num) {
+        long newNum = num;
+        return newNum;
+    }
+
+    public static void main(String[] args) {
+        long num = 44;
+        int x = 44;
+        if ((int)x == (long)num) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
--- src/test/java/JavaProblemsTest.java
+++ src/test/java/JavaProblemsTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.apache.commons.lang3.math.NumberUtils;
+
 
 public class JavaProblemsTest {
 
@@ -13,5 +15,23 @@ public class JavaProblemsTest {
         int expected = 10;
         int actual = JavaProblems.addInts(5, 5);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIntToLong() {
+        long expected = 33;
+        long actual = JavaProblems.intToLong(33);
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        long num = 44;
+        int x = 44;
+        if (x == (int) x) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
     }
 }
